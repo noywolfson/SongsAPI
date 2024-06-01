3cf1fc9fb08689cfd87203bd22a7e59b35a20605
noywolfson <noy@wolfson.co.il>
2024-06-01 21:35:13 +0300

    fix index

--- init-db/init.sql
+++ init-db/init.sql
@@ -1,3 +1,4 @@
-CREATE TABLE songs (index INT, name TEXT, artist TEXT, release_date INT, length FLOAT, popularity INT, danceability FLOAT);
+CREATE SEQUENCE song_index START 5485;
+CREATE TABLE songs (index INTEGER DEFAULT nextval('song_index') PRIMARY KEY, name TEXT, artist TEXT, release_date INT, length FLOAT, popularity INT, danceability FLOAT);
 
 COPY songs FROM '/docker-entrypoint-initdb.d/data.csv' DELIMITER ',' CSV HEADER;
